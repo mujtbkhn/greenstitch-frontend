610b99abdf5afd01c32fdad85fc4752189487704
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-06-01 18:32:04 +0530

    added backend url

--- src/apis/todo.jsx
+++ src/apis/todo.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "http://localhost:3000";
+const URL = "https://greenstitch-backend.onrender.com";
 
 export const createTodo = async (title, description) => {
   try {
